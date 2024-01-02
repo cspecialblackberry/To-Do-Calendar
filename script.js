96ad6e67ea2237c27e48070fe4a6f553fca8fbc0
Hall Family <charliesamhall@icloud.com>
2024-01-02 12:15:39 -0500

    optimized save task feature

--- script.js
+++ script.js
@@ -50,13 +50,16 @@ let hour5TextArea = document.querySelector('#hour5TextArea')
 const hourDivArray = [hour9Div, hour10Div, hour11Div, hour12Div, hour1Div, hour2Div, hour3Div, hour4Div, hour5Div]
 const hourDivIndex = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 const textAreaArray = [hour9TextArea, hour10TextArea, hour11TextArea, hour12TextArea,hour1TextArea,hour2TextArea,hour3TextArea,hour4TextArea,hour5TextArea]
+let taskIndex
+
+currentDateDisplayP.innerHTML = dayjs().format('MM/DD/YYYY')
 
 const applyTextAreaColor = (number, index) => {
-    if(number < dayjs().hour()){
-        hourDivArray[index].setAttribute('class', 'row time-block past')
-    }else if (number = dayjs().hour()){
+    // if(number < dayjs().hour()){
+    //     hourDivArray[index].setAttribute('class', 'row time-block past')
+    if (number === dayjs().hour()){
         hourDivArray[index].setAttribute('class', 'row time-block present')
-    }else{
+    }else if (number > dayjs().hour()){
         hourDivArray[index].setAttribute('class', 'row time-block future')
     }
 }
@@ -69,57 +72,23 @@ const setSavedTasks = (textArea, index) => {
 
 textAreaArray.forEach(setSavedTasks)
 
-currentDateDisplayP.innerHTML = dayjs().format('DD/MM/YYYY')
+const textAreaClassCheck = (textArea) => {
+    return textArea.classList[0]===taskIndex
+}
 
 const saveTask = (event) => {
-    console.log(event)
+    taskIndex = event.target.classList[0]
+    inputTextArea = textAreaArray.find(textAreaClassCheck)
+    localStorage.setItem('Task' + taskIndex, inputTextArea.value)
 }
 
 hour9Button.addEventListener('click', saveTask)
+hour10Button.addEventListener('click', saveTask)
+hour11Button.addEventListener('click', saveTask)
+hour12Button.addEventListener('click', saveTask)
+hour1Button.addEventListener('click', saveTask)
+hour2Button.addEventListener('click', saveTask)
+hour3Button.addEventListener('click', saveTask)
+hour4Button.addEventListener('click', saveTask)
+hour5Button.addEventListener('click', saveTask)
 
-
-const saveTask9 = (event) => {
-    localStorage.setItem('Task' + 9, hour9TextArea.value)
-}
-
-const saveTask10 = (event) => {
-    localStorage.setItem('Task' + 10, hour10TextArea.value)
-} 
-
-const saveTask11 = (event) => {
-    localStorage.setItem('Task' + 11, hour11TextArea.value)
-} 
-
-const saveTask12 = (event) => {
-    localStorage.setItem('Task' + 12, hour12TextArea.value)
-} 
-
-const saveTask1 = (event) => {
-    localStorage.setItem('Task' + 1, hour1TextArea.value)
-} 
-
-const saveTask2 = (event) => {
-    localStorage.setItem('Task' + 2, hour2TextArea.value)
-} 
-
-const saveTask3 = (event) => {
-    localStorage.setItem('Task' + 3, hour3TextArea.value)
-} 
-
-const saveTask4 = (event) => {
-    localStorage.setItem('Task' + 4, hour4TextArea.value)
-} 
-
-const saveTask5 = (event) => {
-    localStorage.setItem('Task' + 5, hour5TextArea.value)
-} 
-
-hour9Button.addEventListener('click', saveTask9)
-hour10Button.addEventListener('click', saveTask10)
-hour11Button.addEventListener('click', saveTask11)
-hour12Button.addEventListener('click', saveTask12)
-hour1Button.addEventListener('click', saveTask1)
-hour2Button.addEventListener('click', saveTask2)
-hour3Button.addEventListener('click', saveTask3)
-hour4Button.addEventListener('click', saveTask4)
-hour5Button.addEventListener('click', saveTask5)
